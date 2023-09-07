7c7f6003de2f3dc4cf011c3518801c0656f15e8b
Matsumoto Kenta <Kenta_Matsumoto_Z@dk.daihatsu.co.jp>
2023-09-07 16:48:18 +0900

    aaa

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,28 @@
+Index!!!
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ブログ</title>
+</head>
+
+<body>
+    <header>
+    </header>
+    <main>
+        <form action="/posts" method="post">
+            @csrf
+            <input type="text" name="title" />
+            <input type="text" name="content" />
+            <button type="submit">
+                送信
+            </button>
+        </form>
+    </main>
+    <footer>
+    </footer>
+</body>
+
+</html>
